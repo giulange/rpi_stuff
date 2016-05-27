2bceb5d1e86d816f3bd5fc88ab8b965f1bdcb568
giuliano from rpi-cam <gyuliano@libero.it>
2016-05-27 22:12:19 +0000

    scripts on keyes led + cam led from gpio

---
+++ cam_led_gpio.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#
+#http://www.raspberrypi-spy.co.uk/2013/05/how-to-disable-the-red-led-on-the-pi-camera-module/
+
+import time
+import RPi.GPIO as GPIO
+ 
+# Use GPIO numbering
+GPIO.setmode(GPIO.BCM)
+ 
+# Set GPIO for camera LED
+# Use 5 for Model A/B and 32 for Model B+
+CAMLED = 32 
+ 
+# Set GPIO to output
+GPIO.setup(CAMLED, GPIO.OUT, initial=False) 
+ 
+# Five iterations with half a second
+# between on and off
+for i in range(5):
+ GPIO.output(CAMLED,True) # On
+ time.sleep(0.5)
+ GPIO.output(CAMLED,False) # Off
+ time.sleep(0.5)
+
---
+++ led_on.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(3,GPIO.OUT)
+
+GPIO.output(3,True)
+
