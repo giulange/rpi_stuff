ee3e378cecb2ca988a5086ff8106329200785b66
giulange <gyuliano@libero.it>
2016-06-26 22:19:15 +0000

    note on flex cable replacement & IR led config

--- picamera/05_recording_video_to_network_stream__reverse.py
+++ picamera/05_recording_video_to_network_stream__reverse.py
@@ -1,16 +1,21 @@
 # run as :: python3.4 05_*py
 # then view in vlc :: file --> open network --> URL=tcp/h264://192.168.1.233:8001/
+# sometimes the socket remains opened, try to recognize pid with
+# ps -fA | grep python # useless
+# sudo netstat -tulpn  # useless
 
 import socket
 import time
 import picamera
+
+PORT = 8001
 
 with picamera.PiCamera() as camera:
     camera.resolution = (640, 480)
     camera.framerate = 24
 
     server_socket = socket.socket()
-    server_socket.bind(('0.0.0.0', 8000))
+    server_socket.bind(('0.0.0.0', PORT))
     server_socket.listen(0)
 
     # Accept a single connection and make a file-like object out of it
