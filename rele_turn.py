69c6b0b016d3d3bcb24861e60a9599b0a2c9a645
giulange <gyuliano@libero.it>
2018-04-03 17:48:30 +0000

    added videosorveglianza notes

--- rele_turn.py
+++ rele_turn.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
 
 # Command:
-#  ./rele_turn.py pin on/off
+#  ./rele_turn.py pin on/off rpiv
 #
-# where on and off means normally closed and opened (because cables
-# were mounted to be normally closed as default).
+# where:
+#    - pin is the GPIO pin number
+#    - on and off means normally closed and opened (because cables
+# were mounted to be normally closed as default)
+#    - rpiv is the last 8-bit IP number (e.g. 36 for parents)
 #
 # Example:
 #  rele_turn.py  4 1 # turn on  p-supply rpi-cam + tinesyne-amp
 #  rele_turn.py 17 1 # turn on  p-supply rpi-studio
-#  rele_turn.py 27 1 # turn on  empty
-#  rele_turn.py 22 1 # turn on  empty
-#  rele_turn.py  4 0 # turn off p-supply rpi-cam + tinesyne-amp
-#  rele_turn.py 17 0 # turn off p-supply rpi-studio
-#  rele_turn.py 27 0 # turn off empty
-#  rele_turn.py 22 0 # turn off empty
+#  rele_turn.py  4 0 36 # turn off p-supply rpi-cam + tinesyne-amp
+#  rele_turn.py 17 0 36 # turn off p-supply rpi-studio
+
+# this is useful to poweroff the rpiv before to turn off the power supply:
+from subprocess import call
 
 import RPi.GPIO as GPIO
 import time
@@ -27,11 +29,19 @@ PIN_RELE = int(sys.argv[1])
 GPIO.setmode(GPIO.BCM)   # referring to the pins by the "Broadcom SOC channel" number, these are the numbers after "GPIO"
 #GPIO.setmode(GPIO.BOARD)# referring to the pins by the number of the pin the the plug
 
+# PARs
+PAUSE = 3
+
+IP = 'pi@192.168.1.' + sys.argv[3]
+
 # Set GPIO to output
 GPIO.setup(PIN_RELE, GPIO.OUT, initial=True)
 #GPIO.setup(PIN_RELE,GPIO.OUT)
 
 if int(sys.argv[2])==0:
+  print "powering off the rpiv:"
+  call(['ssh',IP,'sudo poweroff'])
+  time.sleep( PAUSE )
   print "Rele :: opened contact"
   GPIO.output(PIN_RELE,GPIO.LOW)
 else:
